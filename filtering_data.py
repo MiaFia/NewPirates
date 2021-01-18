c88c8e9c917c282dc7244741a7012644473d2af8
Sophia K <e.s.klaussner@students.uu.nl>
2021-01-18 17:09:33 +0100

    Lol we don't need this but gh won't lemme pull

--- filtering_data.py
+++ filtering_data.py
@@ -4,13 +4,13 @@ import json
 #First filter the file in the shell using commands gc .\People\People\A_people.json | sls "citizenship" |sls "birthYear" |sls "deathYear" >.\A_people_filtered.json
 
 #Then load it into Python as a file
-with open ('A_dutch_people_filtered.json') as file:
+with open ('B_people_Dutch.json') as file:
     real_people= json.load(file)
 
-with open('people_a.csv', 'w') as f:
-    f.write('Citizenship, birthYear, deathYear\n')
+# with open('people_a.csv', 'w') as f:
+#     f.write('Citizenship, birthYear, deathYear\n')
 
-for person in real_people:
-    #if ontology/birthYear<onotology/deathYear:
-        #lifetime = deathYear - birthYear
-    f.write(f"{Citizenship['citizenship']},{birthYear['ontology/birthYear']},{deathYear['onotology/deathYear']}\n")
+# for person in real_people:
+#     #if ontology/birthYear<onotology/deathYear:
+#         #lifetime = deathYear - birthYear
+#     f.write(f"{Citizenship['citizenship']},{birthYear['ontology/birthYear']},{deathYear['onotology/deathYear']}\n")
