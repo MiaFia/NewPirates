9f4eb5ea37ca22242e9af67ace01319ef778a853
Sophia K <e.s.klaussner@students.uu.nl>
2021-01-18 15:54:32 +0100

    Just to not run into issues

--- filtering_data.py
+++ filtering_data.py
@@ -4,11 +4,11 @@ import json
 #First filter the file in the shell using commands gc .\People\People\A_people.json | sls "citizenship" |sls "birthYear" |sls "deathYear" >.\A_people_filtered.json
 
 #Then load it into Python as a file
-with open ('A_dutch_people_filtered.json') as file:
-    real_people= (json.load(file))
+with open ('A_people_filteredh.json') as file:
+    real_people= json.load(file)
 
-# with open('people_a.csv', 'w') as f:
-#     f.write('Citizenship, birthYear, deathYear\n')
+with open('people_a.csv', 'w') as f:
+    f.write('Citizenship, birthYear, deathYear\n')
 
 # for person in real_people:
 #     if birthYear<deathYear:
