8db29a1e9d2f11cb70d572a020248d305d691b12
Johanna <johanna.annau@gmail.com>
2021-01-18 16:38:33 +0100

    It works. with list

--- filtering_data.py
+++ filtering_data.py
@@ -1,11 +1,11 @@
 #Settings CODE
 import json
 
-#First filter the file in the shell using commands gc .\People\People\A_people.json | sls "citizenship" |sls "birthYear" |sls "deathYear" >.\A_people_filtered.json
-
+#First filter the file in the shell using commands gc .\People\People\A_people.json | sls "citizenship" |sls "birthYear" |sls "deathYear" %{$_.Line}>.\A_people_filtered.json
+#Then manually add the square brackets and save it in utf-8
 #Then load it into Python as a file
-with open ('A_dutch_people_filtered.json') as file:
-    real_people= json.load(file)
+with open ('A_dutch_people_filtered.json', encoding= 'utf-8') as file:
+    real_people= json.loads(file.read().replace('\n',''))
 
 
 with open('people_a.csv', 'w') as f:
@@ -22,5 +22,8 @@ list_of_dutch_a = []
 
 for person in real_people:
     birth= person['ontology/birthYear']
-    death = person['onotlogy/deathYear']
-    list_of_dutch_a.append(birth,death)
+    death = person['ontology/deathYear']
+    #and add those to a list!
+    list_of_dutch_a.append({'birth': birth, 'death': death})
+
+print(list_of_dutch_a)
