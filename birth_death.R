3a4bb2f0bebd632efe673537fd1e9b89e7318683
Johanna <johanna.annau@gmail.com>
2021-01-19 10:58:09 +0100

    Tried Shell loop

--- birth_death.R
+++ birth_death.R
@@ -2,3 +2,9 @@ library (tidyverse)
 life_df <- read_csv('people_a.csv')
 span_df <- mutate (life_df, 'lifespan' = deathYear- birthYear)
 #this is the lifespan of dutch people with A now!
+
+library(dplyr)
+library(readr)
+life_df <- list.files(path="", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
