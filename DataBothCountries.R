ba39ec1c9ebebed8da717fb2e4f52ff4b87c5d65
Sophia K <e.s.klaussner@students.uu.nl>
2021-01-20 16:29:31 +0100

    Data + plot for three countries

--- DataBothCountries.R
+++ DataBothCountries.R
@@ -9,13 +9,32 @@ life_expectancy_years <- read_csv("life_expectancy_years.csv")
 life_expectany_SA <- life_expectancy_years%>%
   filter(country== "South Africa")%>%
   pivot_longer(cols = c('1800':'2100'), names_to="Year", values_to="Expectancy")%>%
-  filter(as.integer(Year)<1931)%>%
+  filter(as.integer(Year)<2050)
+Life_Medians_Means_SA= life_expectany_SA%>%
   summarize(median_SA= median(as.integer(Expectancy)),
             mean_SA= mean(as.integer(Expectancy)))
 
 life_expectancy_NL <- life_expectancy_years%>%
   filter(country== "Netherlands")%>%
   pivot_longer(cols = c('1800':'2100'), names_to="Year", values_to="Expectancy")%>%
-  filter(as.integer(Year)<1931)%>%
+  filter(as.integer(Year)<2051)
+
+life_expectancy_JP <- life_expectancy_years%>%
+  filter(country== "Japan")%>%
+  pivot_longer(cols = c('1800':'2100'), names_to="Year", values_to="Expectancy")%>%
+  filter(as.integer(Year)<2051)
+
+
+Life_medians_Means_NL= life_expectancy_NL%>%
   summarize(median_NL= median(as.integer(Expectancy)),
             mean_NL= mean(as.integer(Expectancy)))
+
+ggplot() +
+  geom_line(data= life_expectancy_NL, aes(x=as.integer(Year), y= Expectancy, colour= 'Netherlands'))+
+  geom_line(data= life_expectany_SA, aes(x= as.integer(Year), y= Expectancy, colour= 'South Africa'))+
+  geom_line(data = life_expectancy_JP, aes(x= as.integer(Year), y= Expectancy, colour= 'Japan'))
+
+
+
+
+
