a50c200a0ca7c1b7c6068707c9ebf733e2499130
Johanna <johanna.annau@gmail.com>
2021-01-20 14:26:41 +0100

    cleaned up a little and made clear where it starts

--- filtering_data.py
+++ filtering_data.py
@@ -1,3 +1,7 @@
+ #Settings CODE
+import json
+
+            #Manually filtered json files
 #First, we write some codes in bash in order to filter the json files for the criterias we were looking for,
  #which are entries that contain the string 'dutch', 'birthYear', and 'deathYear', excluding entries containing
  #containing the string 'fictional', and 'mythology'.
@@ -6,9 +10,6 @@
 #"fictional" | grep -v "mythology" >.\A_people_filteredh.json
 
 #We do this manually for all json files A->Z, changing the names of the files accordingly
-
-#Settings CODE
-import json
 
 #Create a list of alphabet letters
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -37,9 +38,12 @@ with open('people_dutch_real.csv', 'w') as f:
         #possibly some metric for location
         f.write(f'{birth}, {death} \n')
 
+
+            #Automatically filtered files in the Shell
 #We can also loop through the entire folder of people in the shell and get a single json file with all people we want:
 # gci .\People\People\ | gc | sls "dutch" |sls "birthYear" |sls "deathYear"|select -expand Line >.\people_filtered_dutch.json
 #Then we only have to go through the above steps for that one file, after saving it as UTF-8
+real_people =[]
 with open('people_filtered_dutch.json', encoding='utf-8') as file:
         #Run through each dictionary line by line
         for line in file:
