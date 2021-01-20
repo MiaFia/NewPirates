78fc8f3158a49f2d601531fa493fc105cecc2a33
Johanna <johanna.annau@gmail.com>
2021-01-20 10:40:18 +0100

    Edited py for powershell commands

--- birth_death.R
+++ birth_death.R
@@ -1,6 +1,9 @@
 library (tidyverse)
 
 life_all<- read_csv2('people_dutch_real.csv')
-span_df <- mutate (life_all, lifespan = as.integer(deathYear)- as.integer(birthYear))%>%
-  filter(1920>birthYear>1861)
-avr_df <- summarise (span_df, 'avr_span'= mean(lifespan))
+span_df <- mutate (life_a, lifespan = as.integer(deathYear)- as.integer(birthYear))%>%
+  filter(1930>birthYear>1861)
+avr_df <- summarise (span_df, 'avr_span'= mean(lifespan), median_span = median(lifespan))
+ggplot(data=span_df)+
+  aes(x=birthYear, y=lifespan)+
+  geom_point()
--- filtering_data.py
+++ filtering_data.py
@@ -36,3 +36,46 @@ with open('people_dutch_real.csv', 'w') as f:
         death = person['ontology/deathYear']
         #possibly some metric for location
         f.write(f'{birth}, {death} \n')
+
+#We can also loop through the entire folder of people in the shell and get a single json file with all people we want:
+# gci .\People\People\ | gc | sls "dutch" |sls "birthYear" |sls "deathYear"|select -expand Line >.\people_filtered_dutch.json
+#Then we only have to go through the above steps for that one file, after saving it as UTF-8
+with open('people_filtered_dutch.json', encoding='utf-8') as file:
+        #Run through each dictionary line by line
+        for line in file:
+            #Remove white spaces in the ends
+            line = line.strip()
+            #If the end of the list is comma, have the line end there
+            if line[-1] == ',':
+                line = line[:-1]
+            line_dict = json.loads(line)
+            #Append all the lines into the empty list
+            real_people.append(line_dict)
+with open('people_dutch_real.csv', 'w') as f:
+    f.write('birthYear, deathYear\n')
+    for person in real_people:
+        birth= person['ontology/birthYear']
+        death = person['ontology/deathYear']
+        #possibly some metric for location
+        f.write(f'{birth}, {death} \n')
+
+#Then we do the same for South Africa
+#gci .\People\People\ | gc | sls "South Africa*" |sls "birthYear" |sls "deathYear"|select -expand Line >.\people_filtered_SA.json
+with open('people_filtered_SA.json', encoding='utf-8') as file:
+        #Run through each dictionary line by line
+        for line in file:
+            #Remove white spaces in the ends
+            line = line.strip()
+            #If the end of the list is comma, have the line end there
+            if line[-1] == ',':
+                line = line[:-1]
+            line_dict = json.loads(line)
+            #Append all the lines into the empty list
+            real_people.append(line_dict)
+with open('people_SA_real.csv', 'w') as f:
+    f.write('birthYear, deathYear\n')
+    for person in real_people:
+        birth= person['ontology/birthYear']
+        death = person['ontology/deathYear']
+        #possibly some metric for location
+        f.write(f'{birth}, {death} \n')
