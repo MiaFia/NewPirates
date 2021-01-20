f6869801bcdd41bbfbf34549a36cd1f8171a6e08
Johanna <johanna.annau@gmail.com>
2021-01-20 10:54:47 +0100

    both NL and SA, which works from the py file

--- birth_death.R
+++ birth_death.R
@@ -1,9 +1,19 @@
 library (tidyverse)
 
-life_all<- read_csv2('people_dutch_real.csv')
-span_df <- mutate (life_a, lifespan = as.integer(deathYear)- as.integer(birthYear))%>%
-  filter(1930>birthYear>1861)
-avr_df <- summarise (span_df, 'avr_span'= mean(lifespan), median_span = median(lifespan))
+NLlife_all<- read_csv2('people_dutch_real.csv')
+NLspan_df <- mutate (life_a, lifespan = as.integer(deathYear)- as.integer(birthYear))%>%
+  filter(birthYear>1861)
+NLavr_df <- summarise (span_df, 'avr_span'= mean(lifespan), median_span = median(lifespan))
+ggplot(data=span_df)+
+  aes(x=birthYear, y=lifespan)+
+  geom_point()
+
+#And the same for South Africans
+
+SAlife_all<- read_csv2('people_SA_real.csv')
+SA_span_df <- mutate (life_a, lifespan = as.integer(deathYear)- as.integer(birthYear))%>%
+  filter(birthYear>1861)
+SA_avr_df <- summarise (span_df, 'avr_span'= mean(lifespan), median_span = median(lifespan))
 ggplot(data=span_df)+
   aes(x=birthYear, y=lifespan)+
   geom_point()
